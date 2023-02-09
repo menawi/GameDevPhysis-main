4ce0708ad47e7c63d5ea1c8ae342e814950b06de
menawi <omar.menawi@gmail.com>
2023-02-08 23:50:28 -0500

    s

--- Ball/JS/Canvas.js
+++ Ball/JS/Canvas.js
@@ -1,5 +1,3 @@
-
-
 const canvas = document.getElementById("mycanvas")
 const context = canvas.getContext('2d');
 
--- Ball/JS/Shape.js
+++ Ball/JS/Shape.js
@@ -81,6 +81,8 @@ function animate() {
   requestAnimationFrame(animate)
 }
 
+// console.log('hello');
+
 
 animate()
 
