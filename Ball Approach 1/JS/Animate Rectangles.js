9f9704839e0a761a6607b0765ab2fa46e787cef9
menawi <omar.menawi@gmail.com>
2023-02-11 23:51:55 -0500

    e

--- Ball Approach 1/JS/Animate Rectangles.js
+++ Ball Approach 1/JS/Animate Rectangles.js
@@ -3,9 +3,7 @@
 
 
 function animate() {
-  // // must clear canvas before every reDraw (experiment changing the value !)
   context.clearRect(0, 0, canvas.width, canvas.height);
-
   // // red rectangle
   // redRectangle.move(redRectangle.dx, redRectangle.dy);
   // redRectangle.draw(context);
@@ -14,18 +12,8 @@ function animate() {
   // yellowRectangle.move(yellowRectangle.dx, yellowRectangle.dy);
   // yellowRectangle.draw(context);
 
-  //   circles
-
-  blueCircle.move(blueCircle.dx, blueCircle.dy);
-  blueCircle.draw(context);
-
-  greenCircle.move(greenCircle.dx, greenCircle.dy);
-  greenCircle.draw(context);
-
-  // requestanimationframe
   requestAnimationFrame(animate);
 }
 
-// console.log('hello');
 
 window.requestAnimationFrame(animate);
